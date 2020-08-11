3d83fc3272cd5685ebc7f5a1b2b5090422087002
Michael Parlee <michael.parlee@dexcom.com>
2020-08-10 17:16:05 -0700

    adds arguments

--- run-ktlint.sh
+++ run-ktlint.sh
@@ -10,4 +10,4 @@ cd ..
 
 changed_kotlin_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*kt$" )
 echo $changed_kotlin_files
-.cache/ktlint $changed_kotlin_files
+.cache/ktlint $@ $changed_kotlin_files
